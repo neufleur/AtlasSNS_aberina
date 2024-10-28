b05591c633b780eb53c9d44a00c2930ff3117d4c
阿部莉奈 <rina7462abe@gmail.com>
2024-10-28 22:34:42 +0900

    fflistレイアウト作成中

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -6,25 +6,31 @@
                 <!-- 一覧表示 -->
                 <form action='/follow-list' method="get">
                 @csrf
-                <div class="follow-images">
+                <div class="ff-images">
                 <h2>Follow list</h2>
                       <!-- foreach 順番に取り出す($配列　as $値を入れる変数(単数)) -->
         <!-- フォローしてる画像のid(ユーザー)　==　ログインしているユーザーの画像　2つの値が異なるかどうかを確認　-->
+        <div class="f-imag">
                 @foreach ($images as $images)
                 @if($images->id !== Auth::user()->$images)
-                <a href="{{ url('/profile-users',$images->id)}}"><img src="{{ asset('storage/images/' . $images->images) }}"></a>
+                <a href="{{ url('/profile-users',$images->id)}}"><img src="{{ asset('storage/images/' . $images->images) }}" width="90px" height="90px"></a>
                 @endif
                @endforeach
+               </div>
                 </div>
 
-                <div class="follow-post">
+                <div class="ff-post">
                 @foreach ($post as $post)
-                <a href="{{ url('/profile-users',$post->user->id)}}"><img src="{{ asset('storage/images/' . $post->user->images) }}"></a>
+                <div class="f-imag">
+                <a href="{{ url('/profile-users',$post->user->id)}}"><img src="{{ asset('storage/images/' . $post->user->images) }}"width="90px" height="90px"></a>
+                </div>
                 <br>{{$post->user->username}}</br>
                 <br>{{$post->created_at}}</br>
                 <br>{{$post->post}}</br>
                  @endforeach
-               </div>
+                 
+                 </div>
+               
 
 
                 </form>
--- resources/views/follows/followerList.blade.php
+++ resources/views/follows/followerList.blade.php
@@ -3,21 +3,24 @@
 @section('content')
                 <!-- 一覧表示 -->
                 <form action='/follower-list' method="get">
-
                 @csrf
-                <div class="follower-images">
+                <div class="ff-images">
+                <h2>Follower list</h2>
                         <!-- foreach 順番に取り出す($配列　as $値を入れる変数(単数)) -->
                 <!-- フォローしてる画像のid(ユーザー)　==　ログインしているユーザーの画像　2つの値が異なるかどうかを確認　-->
-                <h2>Follower list</h2>
+                <div class="f-imag">
                         @foreach ($images as $images)
                         @if($images->id !== Auth::user()->$images)
-                        <a  href="{{ url('/profile-users',$images->id)}}"> <img src="{{ asset('storage/images/' . $images->images) }}"></a>
+                        <a  href="{{ url('/profile-users',$images->id)}}"> <img src="{{ asset('storage/images/' . $images->images) }}" width="90px" height="90px"></a>
                         @endif
                 @endforeach
+                    </div>
                         </div>
-                        <div class="follower-post">
+                        <div class="ff-post">
                         @foreach ($post as $post)
-                        <a href="{{ url('/profile-users',$post->user->id)}}"><img src="{{ asset('storage/images/' . $post->user->images) }}"></a>
+                        <div class="f-imag">
+                        <a href="{{ url('/profile-users',$post->user->id)}}"><img src="{{ asset('storage/images/' . $post->user->images) }}" width="90px" height="90px"></a>
+                        </div>
                         <br>{{$post->user->username}}</br>
                         <br>{{$post->created_at}}</br>
                         <br>{{$post->post}}</br>
